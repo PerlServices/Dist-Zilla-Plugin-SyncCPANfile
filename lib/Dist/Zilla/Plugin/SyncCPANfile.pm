f7247470ac3f1c9c7663ae8edd0912c2352d969b
reneeb <info@perl-services.de>
2023-06-28 12:12:47 +0200

    add feature to bump required versions to a (more) secure release
    If you don't care about minimum versions of dependencies, you might list a version that has known vulnerabilities (and CPAN::Audit knows them).
    So your cpanfile might trigger some CPAN::Audit warnings. To avoid this, SyncCPANfile can set the minimum required version to the
    version that is known to be fixed.

--- lib/Dist/Zilla/Plugin/SyncCPANfile.pm
+++ lib/Dist/Zilla/Plugin/SyncCPANfile.pm
@@ -7,12 +7,21 @@ package Dist::Zilla::Plugin::SyncCPANfile;
 use strict;
 use warnings;
 
+use version;
+
 use Moose;
 use namespace::autoclean;
 use Path::Tiny;
+use CPAN::Audit;
 
 with qw(
   Dist::Zilla::Role::AfterBuild
+);
+
+has cpan_audit => (
+  is  => 'ro',
+  isa => 'Bool',
+  default => 0,
 );
 
 has filename => (
@@ -47,6 +56,8 @@ sub after_build {
 sub _get_cpanfile {
     my ($self) = @_;
 
+    my $audit = CPAN::Audit->new;
+
     my $zilla = $self->zilla;
     my $prereqs = $zilla->prereqs;
  
@@ -67,7 +78,21 @@ sub _get_cpanfile {
             $str .= $prefix;
             
             for my $module ( sort $req->required_modules ) {
-                my $version = $req->requirements_for_module( $module );
+                my $version = $req->requirements_for_module( $module ) || 0;
+
+                if ( $self->cpan_audit ) {
+                    my $min_version            = _audit( $audit, $module, $version );
+                    my $vuln_version_requested = $min_version && (
+                        version->new( $version ) < version->new( $min_version )
+                    );
+
+                    if ( $version == 0 && $vuln_version_requested ) {
+                        $version = $min_version;
+                    }
+                    elsif ( $vuln_version_requested ) {
+                        $self->log( "Prereq $module $version is vulnerable" );
+                    }
+                }
 
                 $str .= sprintf qq~%s%s "%s" => "%s";\n~,
                     $indent,
@@ -81,6 +106,25 @@ sub _get_cpanfile {
     }
 
     return $str;
+}
+
+sub _audit {
+    my ($audit, $module, $version) = @_;
+
+    my $result        = $audit->command( 'module', $module, $version );
+    my ($module_data) = values %{ $result->{dists} || {} };
+
+    my @versions;
+    for my $advisory ( @{ $module_data->{advisories} || [] } ) {
+        my ($fixed_version) = ( $advisory->{fixed_versions} // '' ) =~ m{(v?[0-9]+(?:\.[0-9]+){0,2})};
+        next if !$fixed_version;
+
+        my $version_object = version->new( $fixed_version );
+        push @versions, $version_object;
+    }
+
+    my ($min_version) = sort { $a <=> $b } @versions;
+    return $min_version;
 }
 
 __PACKAGE__->meta->make_immutable;
@@ -121,6 +165,28 @@ You can define your own comment.
     [SyncCPANfile]
     comment  = This is my cpanfile
     comment  = line 2
+
+=head2 cpan_audit
+
+When I<cpan_audit> is enabled, the required module version is not defined (or 0),
+and the module has vulnerabilities, the "fixed version" storied in L<CPAN::Audit>
+is used as a minimum version.
+
+  [SyncCPANfile]
+  cpan_audit = 1
+
+  [Prereqs]
+  ExtUtils::MakeMaker = 0
+
+L<ExtUtils::MakeMaker> has a vulnerability in versions E<lt>= 7.21. As the minimum
+version in the I<dist.ini> is 0 and I<cpan_audit> is enabled, the I<cpanfile>
+will use 7.22 as the minimum version (as of June 2023).
+
+As this depends on the I<CPAN::Audit> database, you should update I<CPAN::Audit>
+regularly.
+
+For dependencies where a minimum version is defined and the defined version is
+vulnerable a warning is shown.
 
 =head1 SEE ALSO
 
